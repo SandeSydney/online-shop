6daf5c67339398bcb974d7e992504c8d31688622
SandeSydney <sandesydney01@gmail.com>
2022-11-05 23:20:01 +0300

    Refactoring with custom Hook

--- src/Components/Electronics/Electronics.jsx
+++ src/Components/Electronics/Electronics.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import useItemsGetter from '../../Hooks/useItemsGetter'
+import Item from '../SingleItem/Item'
 
 function Electronics() {
-  return (
-    <div>Electronics</div>
-  )
+  const electronics = useItemsGetter('https://fakestoreapi.com/products/category/electronics')
+  return electronics
 }
 
 export default Electronics
