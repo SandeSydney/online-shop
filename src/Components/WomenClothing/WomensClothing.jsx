546162e476c949c4026b4c706ec067dc3867015b
SandeSydney <sandesydney01@gmail.com>
2022-11-05 23:19:08 +0300

    Refactoring with custom hook

--- src/Components/WomenClothing/WomensClothing.jsx
+++ src/Components/WomenClothing/WomensClothing.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
+import useItemsGetter from '../../Hooks/useItemsGetter'
 
 function WomensClothing() {
-  return (
-    <div>WomensClothing</div>
-  )
+  const womensclothing = useItemsGetter("https://fakestoreapi.com/products/category/women's clothing")
+  return womensclothing
 }
 
 export default WomensClothing
